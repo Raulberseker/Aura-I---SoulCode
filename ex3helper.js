2aa3074affe5c7f9ba940d8b74869f3cd5fea5f3
Raul Ferraz <128731015+Raulberseker@users.noreply.github.com>
2023-04-24 22:01:03 -0300

    Create ex3helper.js

---
+++ ex3helper.js
@@ -0,0 +1,17 @@
+({
+	getAccounts : function(component) {
+        let action = component.get("c.getlistacc");
+        
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            console.log(state);
+            if(state =="SUCCESS"){
+                component.set("v.accs", response.getReturnValue());
+                console.log(response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+        
+		
+	}
+})
